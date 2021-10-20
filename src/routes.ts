323e4d8d742e7c9d6547d5fd40989a5e8da50bd4
Gustavo <gustavo.prog40@gmail.com>
2021-10-20 18:24:39 -0300

    get route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express'
+import { Response, Router } from 'express'
 import { AuthenticateuserController } from './controllers/AuthenticateuserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
@@ -10,5 +10,8 @@ route.post('/authenticate', new AuthenticateuserController().handle)
 route.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
 route.get('/messages/last3', new GetLast3MessageServiceController().handle)
 route.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
+route.get('/', (res: Response) => {
+  res.status(200).send({ API: 'API Running on heroko!' })
+})
 
 export { route }
